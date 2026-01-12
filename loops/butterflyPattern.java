95e19591149c57c3e132b1d0345791f7a3d745d0
Dhruv481821 <sharmadhruv2403@gmail.com>
2026-01-12 20:58:59 +0530

    Add_loop_pattern_programs

--- loops/butterflyPattern.java
+++ loops/butterflyPattern.java
@@ -4,7 +4,7 @@ public class butterflyPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the number of row: ");
+        System.out.print("Enter the numbe of rows: ");
         int rows = sc.nextInt();
 
         //upper half
@@ -12,6 +12,31 @@ public class butterflyPattern {
             for(int j=1; j<=i; j++) {
                 System.out.print("*");
             }
+
+            int spaces = 2*(rows-i);
+            for(int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //lower half
+        for(int i = rows; i>= 1; i--) {
+            for(int j =1; j<=i; j++) {
+                System.out.print("*");
+            }
+
+            for(int j =1; j<=2*(rows-i); j++) {
+                System.out.print(" ");
+            }
+
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
